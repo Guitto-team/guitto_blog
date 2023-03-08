b23d6293516837bea725a1bf0e77f1c4260898f6
Toshiki Isawa <toshiki.isawa@gmail.com>
2023-03-08 17:56:48 +0900

    GTMタグ実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { AnimatePresence } from 'framer-motion'
+import TagManager from 'react-gtm-module'
 
 import '../styles/reset.scss'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps, router }) {
+  // Google Tag Manager start
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-W3ZXMH8' })
+  }, [])
+
+  useEffect(() => {
+    document.body.classList?.remove('loading')
+  }, [])
+  // Google Tag Manager end
+
   return (
     <>
       <AnimatePresence exitBeforeEnter onExitComplete={() => window.scrollTo(0, 0)}>
